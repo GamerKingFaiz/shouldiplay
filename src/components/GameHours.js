8a0ef10f8da25bf8f4f8efcd1a29469efd78449a
GamerKingFaiz <gamerkingfaiz@gmail.com>
2021-12-29 20:25:26 -0800

    Shortened hour label strings

--- src/components/GameHours.js
+++ src/components/GameHours.js
@@ -4,6 +4,20 @@ const GameHours = ({ value, subtitle }) => {
   const theme = useTheme();
   const mobile = useMediaQuery(theme.breakpoints.down("sm"));
 
+  switch (subtitle) {
+    case "Main Story":
+      subtitle = "MAIN";
+      break;
+    case "Main + Extra":
+      subtitle = "+EXTRAS";
+      break;
+    case "Completionist":
+      subtitle = "COMPLETE";
+      break;
+    default:
+      subtitle = subtitle.toUpperCase();
+  }
+
   return (
     <Box
       display={"flex"}
@@ -12,7 +26,7 @@ const GameHours = ({ value, subtitle }) => {
       alignItems={"flex-start"}
     >
       <Typography fontSize={mobile ? 9 : 11} fontWeight={500}>
-        {subtitle.toUpperCase()}
+        {subtitle}
       </Typography>
       <Typography fontSize={mobile ? 15 : 20} fontWeight={700}>
         {value}
