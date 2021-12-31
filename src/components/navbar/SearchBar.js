5238a5450c64ed4ede0828fe63c50eba48076360
GamerKingFaiz <gamerkingfaiz@gmail.com>
2021-12-30 16:06:25 -0800

    Rounded corners and changed focus/hover styles.

--- src/components/navbar/SearchBar.js
+++ src/components/navbar/SearchBar.js
@@ -7,15 +7,44 @@ import {
   InputLabel,
   OutlinedInput,
 } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  form: {
+    margin: 16,
+    minWidth: "30%",
+    "& .MuiInputLabel-outlined.Mui-focused": {
+      color: "#DCDCFF",
+      marginLeft: -8,
+    },
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#DCDCFF",
+    },
+    "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+      borderColor: "rgba(255, 255, 255, 0.50)",
+    },
+  },
+}));
+
 const SearchBar = ({ handleChange, onChange }) => {
+  const classes = useStyles();
+
   return (
-    <FormControl variant="outlined" sx={{ margin: "16px", minWidth: "30%" }}>
-      <InputLabel htmlFor="sipSearchbar">Search for games...</InputLabel>
+    <FormControl variant="outlined" className={classes.form}>
+      <InputLabel
+        htmlFor="sipSearchbar"
+        sx={{ color: "rgba(220, 220, 255, .7)" }}
+      >
+        Search for games...
+      </InputLabel>
       <OutlinedInput
         id="sipSearchbar"
         type="search"
+        sx={{ borderRadius: "60px", marginLeft: -1, paddingRight: 3 }}
+        onKeyDown={handleChange}
+        onChange={(e) => onChange(e.target.value)}
+        label="Search for games..."
         endAdornment={
           <InputAdornment position="end">
             <IconButton
@@ -23,13 +52,10 @@ const SearchBar = ({ handleChange, onChange }) => {
               edge="end"
               onClick={handleChange}
             >
-              <FontAwesomeIcon icon={faSearch} />
+              <FontAwesomeIcon icon={faSearch} color="#C8D4FF" />
             </IconButton>
           </InputAdornment>
         }
-        onKeyDown={handleChange}
-        onChange={(e) => onChange(e.target.value)}
-        label="Search for games..."
       />
     </FormControl>
   );
