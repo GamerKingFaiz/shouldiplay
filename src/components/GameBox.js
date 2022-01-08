766ecf1b97e67e1444e7bb8bbcabcafb2f5fff7e
GamerKingFaiz <gamerkingfaiz@gmail.com>
2022-01-07 17:09:23 -0800

    Moved OpenCritic API call to the backend

--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -20,35 +20,20 @@ const GameBox = ({ data }) => {
 
   useEffect(() => {
     setLoading(true);
+    // Regex removes adjacent whitespace characters
     const cleanName = data.name.replace(/\s+/g, " ").toLowerCase();
     fetch(`https://shouldiplay-api.herokuapp.com/opencriticid/${cleanName}`)
       .then((response) => response.json())
       .then((json) => {
-        if (json.opencriticid) {
-          fetch(`https://api.opencritic.com/api/game/${json.opencriticid}`)
-            .then((openCriticGame) => openCriticGame.json())
-            .then((openCriticGameJson) => {
-              setOpenCriticGameData(openCriticGameJson);
-              setLoading(false);
-            });
-        } else setLoading(false);
+        if (json.topCriticScore) {
+          setOpenCriticGameData(json);
+          setGameMatched(true);
+        } else {
+          setGameMatched(false);
+        }
+        setLoading(false);
       });
   }, [data.name]);
-
-  useEffect(() => {
-    /**
-     * Comparing the name from the HowLongToBeat API (after stripping adjacent
-     * whitespace) to the name of the first result of the OpenCritic API (after
-     * removing everything except alphanumeric characters and whitespace)
-     */
-    data.name.replace(/\s+/g, " ").toLowerCase() ===
-    openCriticGameData?.name
-      .replace(/[^\w\s]|_/g, "")
-      .replace(/\s+/g, " ")
-      .toLowerCase()
-      ? setGameMatched(true)
-      : setGameMatched(false);
-  }, [data.name, openCriticGameData]);
 
   return (
     <Card className={classes.card}>
